3cff1b20d497d95e3b1696b05d330a11ead951ff
Serafin <leaf466@gmail.com>
2017-04-06 09:23:59 -0700

    Removes sockets pattern in favor of streams - adds console logs for error handling (due to oauth expiring tokens) - adds successful test case for file streaming

--- app.js
+++ app.js
@@ -6,14 +6,13 @@ const oauth = require('oauth')
 const consumer = require('./consumer')
 const cookieParser = require('cookie-parser')
 const session = require('express-session')
-const socket = require('./socket')
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, '/public')))
 app.set('view engine', 'pug')
-app.set('views', './views')
-app.set('/db', './db')
+app.set('views', path.join(__dirname, '/views'))
+// app.set('db', './db')
 
 app.use(cookieParser())
 app.use(session({ 
@@ -36,22 +35,29 @@ app.get('/', (request, response) => {
 })
 
 app.get('/twitter', function(request, response){
-  consumer.get("https://api.twitter.com/1.1/account/verify_credentials.json", 
-    request.session.oauthAccessToken, 
-    request.session.oauthAccessTokenSecret, 
-    (error, data, consumerResponse) => {
-      if (error) {
-        response.status(500).send("Error getting twitter screen name : " + error.data)
-      } else {
-        let parsedData = JSON.parse(data)
+  // consumer.get("https://api.twitter.com/1.1/account/verify_credentials.json", 
+  //   request.session.oauthAccessToken, 
+  //   request.session.oauthAccessTokenSecret, 
+  //   (error, data, consumerResponse) => {
+  //     if (error) {
+  //       console.log('error =====>', error)
+  //       response.status(500).send("Error getting twitter screen name : " + error.data)
+  //     } else {
+        // let parsedData = JSON.parse(data)
 
-        response.setHeader({'Content-Type': 'video/mp4'})
+        response.setHeader('Content-Type', 'video/mp4')
         const fs = require('fs')
-        const rs = fs.createReadStream('LoremIpsum.mp4')
-        rs.pipe(response)
-      } 
-    }
-  )
+        const stream = fs.createReadStream('LoremIpsum.mp4')
+        stream.on('data', data => {
+          response.write(data)
+        })
+
+        stream.on('end', () => {
+          response.end()
+        })
+      // }
+    // }
+  // )
 })
 
 app.get('/favicon.ico', (request, response) => {
--- session/index.js
+++ session/index.js
@@ -6,22 +6,27 @@ const consumer = require('../consumer')
 router.get('/connect', (request, response) => {
   consumer.getOAuthRequestToken( (error, oauthToken, oauthTokenSecret, results) => {
     if(error) {
-      response.send('Error getting OAuth request token : '+error.message, 500)
+      console.log(error)
+      response.status(500).send('Error getting OAuth request token : '+error)
     } else {
       request.session.oauthRequestToken = oauthToken
       request.session.oauthRequestTokenSecret = oauthTokenSecret
+      console.log('\nRequest*\ntoken:', request.session.oauthRequestToken, ', secret:', request.session.oauthRequestTokenSecret, ', verifier:', request.query.oauth_verifier)
+
       response.redirect('https://twitter.com/oauth/authorize?oauth_token='+request.session.oauthRequestToken)
     }
   })
 })
 
 router.get('/callback', (request, response) => {
+  console.log('\nAccess*\ntoken:', request.session.oauthRequestToken, ', secret:', request.session.oauthRequestTokenSecret, ', verifier:', request.query.oauth_verifier)
   consumer.getOAuthAccessToken(
     request.session.oauthRequestToken, 
     request.session.oauthRequestTokenSecret, 
     request.query.oauth_verifier, 
     (error, oauthAccessToken, oauthAccessTokenSecret, results) => {
     if (error) {
+      console.log('\nerror: ', error, '\n\nresults: ', results)
       response.status(500).send('Error getting OAuth access token : ' + error + '['+oauthAccessToken+']'+ '['+oauthAccessTokenSecret+']'+ '['+results+']')
     } else {
       request.session.oauthAccessToken = oauthAccessToken
